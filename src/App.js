d3f404e42cde199426be9494a9e635d0ec82df6a
cburgee <caleb.burge@gmail.com>
2023-08-15 13:27:24 -0500

    added necessary components and psuedocoded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      {/*
+     Header display with title
+     an extra large separate card for current temp and weather conditions.
+     Outer box with to hold 7-day forecast
+     individual cards for each day laid out side-by-side to display week's forecast
+     */}
+    </div>
+  )
 }
 
 export default App
