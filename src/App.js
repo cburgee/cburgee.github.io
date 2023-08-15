ea0de2467c62886fc8925cd5a6525f0d9eb00eff
cburgee <caleb.burge@gmail.com>
2023-08-15 15:21:33 -0500

    api endpoint is functioning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import "./App.css"
 
+async function getMeSomeData() {
+  const dallasLatitude = 32.78
+  const dallasLongitude = -96.81
+  const apiEndpoint = `https://api.openweathermap.org/data/3.0/onecall?lat=${dallasLatitude}&lon=${dallasLongitude}&units=imperial&exclude=hourly,alerts,minutely&appid=${process.env.REACT_APP_API_KEY}`
+  const response = await fetch(apiEndpoint)
+  const data = await response.json()
+  console.log(data)
+}
+getMeSomeData()
 function App() {
   return (
     <div className="App">
@@ -8,6 +17,7 @@ function App() {
      an extra large separate card for current temp and weather conditions.
      Outer box with to hold 7-day forecast
      individual cards for each day laid out side-by-side to display week's forecast
+
      */}
     </div>
   )
